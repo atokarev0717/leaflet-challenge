04c96731eb62c12f39d416f504b5d19f1dc72fe7
Andrey Tokarev <atokarev1@gmail.com>
2021-03-21 17:06:28 -0600

    reading json

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,9 @@
 // store URL to the data json 
 
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+
+// get the json data
+d3.json(url).then(data => {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
